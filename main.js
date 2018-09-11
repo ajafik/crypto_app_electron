c8ede65cc85b8e1ce7b31dfbe47b8ca7e9004e66
ajafik <oluwafikayoajayi@gmail.com>
2018-09-11 11:17:50 +0100

    Added basic Menu with click functionality

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron')
   
   // Keep a global reference of the window object, if you don't, the window will
   // be closed automatically when the JavaScript object is garbage collected.
@@ -21,6 +21,25 @@ const {app, BrowserWindow} = require('electron')
       // when you should delete the corresponding element.
       win = null
     })
+
+    var menu = Menu.buildFromTemplate([
+        {
+            label : "Menu",
+            submenu: [
+                {label : "Adjust Notification Value"},
+                {label : "CoinMarketCap"},
+                {
+                    label : "Exit",
+                    click(){
+                        app.quit();
+                    }
+                }
+            ]
+        }
+    ]);
+
+    Menu.setApplicationMenu(menu);
+
   }
   
   // This method will be called when Electron has finished
